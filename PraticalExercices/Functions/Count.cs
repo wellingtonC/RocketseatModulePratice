121abf752680ba65a2317dde303ce66ee83d9a22
wellington carvalho <well.f.carvalho@gmail.com>
2025-11-18 14:05:41 -0300

    Ajsute para utlizacao de length

--- PraticalExercices/Functions/Count.cs
+++ PraticalExercices/Functions/Count.cs
@@ -3,10 +3,11 @@
 internal class Count
 {
     internal void caracteres() {
+        string teste = "";
         Console.Clear();
         Console.WriteLine("Insira a palavra que deseja contar caracteres");
         string palavra = Console.ReadLine();
-        Console.WriteLine("esta palvra contem {0} caracteres\nPressione qualquer tecla para sair", palavra.Count());
+        Console.WriteLine("esta palvra contem {0} caracteres\nPressione qualquer tecla para sair", palavra.Length);
         Console.ReadKey();
     }
 }
